c6fa875256703cb0220bfc70e84ac0a1e0ee1501
Pauline Lesueur <paulinelesueur93@gmail.com>
2020-08-12 11:24:41 +0200

    routes/sauces

--- routes/sauces.js
+++ routes/sauces.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
 
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 const saucesCtrl = require('../controllers/sauces');
 
 router.get('/', auth, saucesCtrl.getAllSauces);
 router.get('/:id', auth, saucesCtrl.getOneSauce);
-router.post('/', auth, saucesCtrl.createSauce);
-router.put('/:id', auth, saucesCtrl.updateSauce);
+router.post('/', auth, multer, saucesCtrl.createSauce);
+router.put('/:id', auth, multer, saucesCtrl.updateSauce);
 router.delete('/:id', auth, saucesCtrl.deleteSauce);
 router.post('/:id/like', auth, saucesCtrl.likeSauce);
 
