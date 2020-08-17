34bb2f4b4d65bf794f11421c0bfab4846ddff5f3
Pauline Lesueur <paulinelesueur93@gmail.com>
2020-08-17 08:27:17 +0200

    installation de helmet

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const path = require('path');
-const xss = require('xss-clean');
+const bodyParser = require('body-parser'); //used to extract the informations of http request and make them usable
+const mongoose = require('mongoose'); //used to connect with the database
+const path = require('path'); //used to upload our images
+const xss = require('xss-clean'); //security plugin used to prevent users from inserting scripts or HTML on inputs
+const helmet = require('helmet'); //global security plugin 
 
 const authRoute = require('./routes/auth');
 const saucesRoute = require('./routes/sauces');
-const app = express();
 
-//connexion à la base de données MongoDB
 mongoose.connect('mongodb+srv://Line:VPjPWFF3ycGvfTQk@cluster0.7otcv.mongodb.net/test?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-//déclaration des headers d'autorisation
-app.use((req, res, next) => { 
+const app = express();
+
+app.use(xss());
+app.use(helmet());
+
+app.use((req, res, next) => { //we declare the headers to allow
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use(xss());
 app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
