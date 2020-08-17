53658c061106c76979576462e35d3e66fbcfedc5
Pauline Lesueur <paulinelesueur93@gmail.com>
2020-08-17 08:00:06 +0200

    installation de xss clean

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const xss = require('xss-clean');
 
 const authRoute = require('./routes/auth');
 const saucesRoute = require('./routes/sauces');
@@ -22,6 +23,7 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(xss());
 app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
