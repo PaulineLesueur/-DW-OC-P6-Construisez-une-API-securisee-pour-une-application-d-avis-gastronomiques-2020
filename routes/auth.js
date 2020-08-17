cafd4712a8a9bef3d167c5b13cca754032a0767b
Pauline Lesueur <paulinelesueur93@gmail.com>
2020-08-17 07:46:55 +0200

    antibruteforce système au login

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const rateLimit = require('express-rate-limit');
 
 const authCtrl = require('../controllers/auth');
+const authLimit = rateLimit({
+    max: 3,
+    windowMs: 5 * 60 * 1000,
+    message: "Nombre maximal d'essais atteint, merci de patienter 5 minutes avant de réessayer"
+});
 
 router.post('/signup', authCtrl.signup);
-router.post('/login', authCtrl.login);
+router.post('/login', authLimit, authCtrl.login);
 
 module.exports = router;
